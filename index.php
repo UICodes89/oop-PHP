a6d6364c9c0a8e147a7868276aaffca3dd2185e8
manoj <manojkshukla87@gmail.com>
2014-10-04 00:58:30 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  //autoload function to load different classes.
+  include"autoload.php";
+  //object create for show class.
+  $object1=new show();
+  $object1->name();
+ 
+?>
---
+++ show.php
@@ -0,0 +1,15 @@
+<?php
+  class show{
+   
+    public function __construct(){
+
+    	echo "Manoj this autoload is working";
+    }
+    public function name(){
+
+    	echo "My name is Manoj Shukla";
+    }
+
+  }
+
+?>
